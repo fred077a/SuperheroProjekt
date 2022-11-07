39be8a227baee29a25580c00d5de661aa0a3fc20
fred077a <fred18q3@stud.kea.dk>
2022-11-07 12:21:37 +0100

    christian commit

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -162,7 +162,7 @@ public class UserInterface {
         String reset = "\u001B[0m";
         if (controller.getSuperheroes().size() > 0)
             for (Superhero superhero: controller.getSuperheroes()) {
-                System.out.printf(red + "Name: %-20s  \u001B[32m Superpower: %-20s \u001B[33m Form: %-12s \u001B[34m Year introduced: %-12s" +
+                System.out.printf(red + "Name: %-20s  \u001B[32m Superpower: %-20s \u001B[33m Form: %-20s \u001B[34m Year introduced: %-12s" +
                                 "\u001B[35m  Strength: %20s"
                         ,red + superhero.getName()
                         ,green + superhero.getForm()
